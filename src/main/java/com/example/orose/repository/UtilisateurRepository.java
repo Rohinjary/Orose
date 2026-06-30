91df2b09b0d1c659d9115016236aabd57810dbed
Rohinjary <andrianimerinanjary@gmail.com>
2026-06-30 08:54:03 +0300

    presque finale

--- src/main/java/com/example/orose/repository/UtilisateurRepository.java
+++ src/main/java/com/example/orose/repository/UtilisateurRepository.java
@@ -9,10 +9,9 @@ import org.springframework.data.jpa.repository.Query;
 import com.example.orose.model.Utilisateur;
 
 public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> {
-    @Query("SELECT u FROM Utilisateur u " +
-            "JOIN u.roles r " +
-            "WHERE r.code = 'TECH'")
+    @Query("SELECT u FROM Utilisateur u LEFT JOIN FETCH u.roles WHERE u.email = :email")
     List<Utilisateur> findAllTechniciens();
 
+    @Query("SELECT u FROM Utilisateur u LEFT JOIN FETCH u.roles WHERE u.email = :email")
     Optional<Utilisateur> findByEmail(String email);
 }
